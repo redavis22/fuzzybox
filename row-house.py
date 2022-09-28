6fc211892a78d3ee668bd6427cfcba4903303bcf
redavis22 <redavis22@gmail.com>
2022-09-28 16:06:17 -0400

    Update row-house.py

--- row-house.py
+++ row-house.py
@@ -8,7 +8,7 @@ Original file is located at
 """
 
 ciphertext = []
-print("Please enter row transposition ciphertext \n")
+print("Hello, Tolu. Please enter row transposition ciphertext \n")
 
 ciphertext = str(input(''))
 

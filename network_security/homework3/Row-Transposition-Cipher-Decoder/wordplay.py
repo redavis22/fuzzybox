099b331f899311501fba4678437b3ac7a0f3fa75
redavis22 <redavis22@gmail.com>
2022-09-29 18:42:50 -0400

    Update wordplay.py

--- network_security/homework3/Row-Transposition-Cipher-Decoder/wordplay.py
+++ network_security/homework3/Row-Transposition-Cipher-Decoder/wordplay.py
@@ -2,27 +2,22 @@ from itertools import permutations
 import enchant
 d = enchant.Dict("en_US")
 
+def spell_check():
+    input1 = []
+    print("Please enter row characters for wordplay: \n")
+    input1 = str( input(''))
+    letter1 = [x.lower() for x in input1]
+    print(letter1)
 
-input1 = []
-print("Please enter row characters for wordplay: \n")
-input1 = str( input(''))
-letter1 = [x.lower() for x in input1]
-print(letter1)
-
-for y in list(permutations(letter1, 6)):
-    z = "".join(y)
-    if len(z) > 2:
-        if d.check(z):
-            print(z)
+    for y in list(permutations(letter1, 6)):
+        z = "".join(y)
+        if len(z) > 2:
+            if d.check(z):
+                print(z)
+               
+while(True):
+    spell_check()
 
 
 
-input2 = "cynicponcfeilos"
-letter2 = [x.lower() for x in input2]
-print(letter2)
 
-for y in list(permutations(letter2, 6)):
-    z = "".join(y)
-    if len(z) > 2:
-        if d.check(z):
-            print(z)

a905e8a33a9b0bb6a196b4e7c81420e502ceb0b2
redavis22 <redavis22@gmail.com>
2022-10-19 17:31:24 -0400

    Update bigdesy.sh

--- network_security/homework4/bigdesy.sh
+++ network_security/homework4/bigdesy.sh
@@ -11,23 +11,23 @@ echo "make 1MB file of zeroes"
 #dd if=/dev/zero of=output.file bs=1024 count=1024
 
 dd if=/dev/zero of=network.txt bs=1024 count=1024
-echo "DES"
+echo "***********************DES***********************"
 time openssl enc -des -a  -in network.txt -out network_DES-out.enc  -k 999999 && time openssl enc -des -a  -in network.txt -out network_DES-out.enc  -k 999999 && time openssl enc -des -a  -in network.txt -out network_DES-out.enc  -k 999999
-echo "3DES"
+echo "***********************3DES***********************"
 time openssl enc -des3 -a -in network.txt -out network_DES3.txt -k 123des321 && time openssl enc -des3 -a -in network.txt -out network_DES3.txt -k 123des321 && time openssl enc -des3 -a -in network.txt -out network_DES3.txt -k 123des321
-echo "AES-128"
+echo "***********************AES-128***********************"
 time openssl enc -aes-128-cbc -a -in network.txt -out networkaes.enc.txt -k 123456 && time openssl enc -aes-128-cbc -a -in network.txt -out networkaes.enc.txt -k 123456 && time openssl enc -aes-128-cbc -a -in network.txt -out networkaes.enc.txt -k 123456
-echo "RC4"
+echo "***********************RC4***********************"
 time openssl enc -rc4 -a -in network.txt -out network_rc4.txt.enc -k 123456 && time openssl enc -rc4 -a -in network.txt -out network_rc4.txt.enc -k 123456 && time openssl enc -rc4 -a -in network.txt -out network_rc4.txt.enc -k 123456
-echo "MD5"
+echo "***********************MD5***********************"
 time openssl dgst -md5 network.txt 
 time openssl dgst -md5 network.txt 
 time openssl dgst -md5 network.txt
-echo "SHA1"
+echo "***********************SHA1***********************"
 time openssl dgst -sha1 network.txt 
 time openssl dgst -sha1 network.txt 
 time openssl dgst -sha1 network.txt
-echo "SHA256"
+echo "***********************SHA256***********************"
 time openssl dgst -sha256 network.txt 
 time openssl dgst -sha256 network.txt 
 time openssl dgst -sha256 network.txt

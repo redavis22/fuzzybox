45d3b120bcafa0dcdd2448e69d6c3b0fdf8e37a2
redavis22 <redavis22@gmail.com>
2022-10-19 17:30:06 -0400

    Update bigdesy.sh

--- network_security/homework4/bigdesy.sh
+++ network_security/homework4/bigdesy.sh
@@ -20,10 +20,16 @@ time openssl enc -aes-128-cbc -a -in network.txt -out networkaes.enc.txt -k 1234
 echo "RC4"
 time openssl enc -rc4 -a -in network.txt -out network_rc4.txt.enc -k 123456 && time openssl enc -rc4 -a -in network.txt -out network_rc4.txt.enc -k 123456 && time openssl enc -rc4 -a -in network.txt -out network_rc4.txt.enc -k 123456
 echo "MD5"
-time openssl dgst -md5 network.txt && time openssl dgst -md5 network.txt && time openssl dgst -md5 network.txt
+time openssl dgst -md5 network.txt 
+time openssl dgst -md5 network.txt 
+time openssl dgst -md5 network.txt
 echo "SHA1"
-time openssl dgst -sha1 network.txt && time openssl dgst -sha1 network.txt && time openssl dgst -sha1 network.txt
+time openssl dgst -sha1 network.txt 
+time openssl dgst -sha1 network.txt 
+time openssl dgst -sha1 network.txt
 echo "SHA256"
-time openssl dgst -sha256 network.txt && time openssl dgst -sha256 network.txt && time openssl dgst -sha256 network.txt
+time openssl dgst -sha256 network.txt 
+time openssl dgst -sha256 network.txt 
+time openssl dgst -sha256 network.txt
 #time openssl rsautl -encrypt -inkey public.pen -pubin -in network.txt -out output.txt
 
